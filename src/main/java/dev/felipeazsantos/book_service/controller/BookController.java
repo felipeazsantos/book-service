e1091e7c3b6b6aa566eabdb1224b538f9b670fef
Felipe Azevedo <felipeasantos10@hotmail.com>
2026-09-18 18:15:17 -0300

    feat: add migrations and db config

--- src/main/java/dev/felipeazsantos/book_service/controller/BookController.java
+++ src/main/java/dev/felipeazsantos/book_service/controller/BookController.java
@@ -5,6 +5,7 @@ import dev.felipeazsantos.book_service.model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +18,12 @@ public class BookController {
     @Autowired
     private InstanceInformationService informationService;
 
+    // http://localhost:8100/book-service/1/BRL
     @GetMapping(value = "/{id}/{currency}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Book findBook() {
+    public Book findBook(
+            @PathVariable("id") Long id,
+            @PathVariable("currency") String currency
+    ) {
         String port = informationService.retrieveServerPort();
 
         return new Book(
